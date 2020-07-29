706b4152e3c734f5ce365113a138eea273ac98f6
KacperGawronski <38177700+KacperGawronski@users.noreply.github.com>
2020-07-29 10:10:51 +0200

    Everything
    It's done and works.

---
+++ make_czolg.js
@@ -0,0 +1,12 @@
+function make_czo³g(nowy){
+    var czo³g, $czo³g;
+    var e, let = [e=function (czo³g){return undefined,NaN,e}];
+    var g±sienice= [let +let[0]()()()()()()][0][6];
+    function dzia³o(milimetry){
+	//todo
+	return dzia³o;
+    }
+    var wie¿yczka= (let +dzia³o(155))[58];
+    nowy= [ (czo³g?! let + czo³g:(let + (czo³g!=$czo³g) ) )[42] + g±sienice  + wie¿yczka];
+    return nowy;
+}
